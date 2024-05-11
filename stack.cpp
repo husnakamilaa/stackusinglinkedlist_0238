a3e00fad8f319da58ac41b5648e58fa90270c693
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-05-11 12:15:41 +0700

    menambahkan decision making if untuk ngecek apakah stack itu kosong atau enggak

--- stack.cpp
+++ stack.cpp
@@ -38,6 +38,10 @@ public :
     // Pop operation: Remove the topmost element from the stack
     void pop()
     {
-    
+        if (isEmpty())
+		{
+			cout << "Stack is empty." << endl;
+			return;
+		}
     }
 };
